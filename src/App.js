58e06327a4a06724babbfe8085dcd86af9656847
Achmad <achmad.a.interns@aux.gojek.com>
2022-08-05 13:18:49 +0700

    feat: try description

--- src/App.js
+++ src/App.js
@@ -6,13 +6,26 @@ function App() {
   return (
     <div className="App">
       <div style={{ display: "flex" }}>
-        <div style={{ flex: 1, marginLeft: '24px' }}>
+        <div style={{ flex: 1, marginLeft: "24px" }}>
           <SolarSystem />
           <Description
             textInput={"Hello World"}
-            attribute={`let's see some sign of time in universe`}
-            description={`This is a description of the text input`}
+            attribute={`I'm`}
+            name={`Human`}
+            description={`Frontend Developer`}
           />
+        </div>
+        <div
+          style={{
+            flex: 2,
+            color: "white",
+            margin: "auto",
+            left: '45%',
+            position: 'absolute',
+            top: '20%',
+          }}
+        >
+          <p>Hello</p>
         </div>
         <Clock />
       </div>
--- src/components/description.jsx
+++ src/components/description.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Description = ({ textInput, attribute, description }) => {
+const Description = ({ textInput, attribute, name, description }) => {
   return (
     <>
       <div style={{ display: "flex", flexDirection: 'column' , color: 'white', fontFamily: 'arial', fontSize: '24px'}}>
         <h3>{textInput}</h3>
-        <>{attribute}</>
-        <>{description}</>
+        <div>{attribute} <span style={{fontSize: '40px', color: 'red'}}>{name}</span></div>
+        <div>{description}</div>
       </div>
     </>
   );
